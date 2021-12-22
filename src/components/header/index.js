a9bc066ecdc489b8535d455d6670caef3c717668
cjpwq <50103094+cjpwq@users.noreply.github.com>
2021-12-22 15:40:24 +0800

    首页开发-标签栏组件

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,44 @@
 import './header.css';
 
-import Scroll from 'utils/scroll';
-
 const CHANGED_CLASS_NAME = 'header-transition';
+const INIT_STATE = 'init';
+const CHANGED_STATE = 'changed';
 
 class Header {
   constructor(el, critical_point, scrollContainer, eventEl = scrollContainer) {
     this.el = el;
+    this.critical_point = critical_point;
 
-    new Scroll(
-      {
-        critical_point,
-        change: () => {
+    // 滚动条所在的容器
+    this.scrollContainer = scrollContainer;
+
+    // 监听滚动事件的元素
+    this.eventEl = eventEl;
+
+    this.setState(INIT_STATE);
+
+    this.bindEvent();
+  }
+
+  // 设置状态
+  setState(state) {
+    this.state = state;
+  }
+
+  // 绑定事件
+  bindEvent() {
+    this.eventEl.addEventListener(
+      'scroll',
+      () => {
+        if (this.needChange()) {
+          this.setState(CHANGED_STATE);
           this.change();
-        },
-        reset: () => {
+        } else if (this.needReset()) {
+          this.setState(INIT_STATE);
           this.reset();
         }
       },
-      scrollContainer,
-      eventEl
+      false
     );
   }
 
@@ -27,9 +46,24 @@ class Header {
     this.el.classList.remove(CHANGED_CLASS_NAME);
   }
 
+  needReset() {
+    return (
+      this.state !== INIT_STATE &&
+      this.scrollContainer.scrollTop <= this.critical_point
+    );
+  }
+
   //   变化
   change() {
     this.el.classList.add(CHANGED_CLASS_NAME);
+  }
+
+  //   需要变化
+  needChange() {
+    return (
+      this.state !== CHANGED_STATE &&
+      this.scrollContainer.scrollTop > this.critical_point
+    );
   }
 }
 
